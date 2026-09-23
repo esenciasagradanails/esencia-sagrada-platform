4930dc63903cd8afa72b48a8a18fde565ec6bdf8
Juan Vargas <juandeveloper19@gmail.com>
2026-09-23 18:57:54 -0500

    feat(presentation): implement AppRouter.tsx following clean architecture patterns

---
+++ src/presentation/routes/AppRouter.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { BrowserRouter, Navigate, Route, Routes, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+import Home from '../views/Home/Home';
+import LoginView from '../views/Login/LoginView';
+import DashboardView from '../views/Dashboard/DashboardView';
+import ProtectedRoute from '../guards/ProtectedRoute';
+import Navbar from '../components/shared/Navbar/Navbar';
+
+const AnimatedRoutes = () => {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginView />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <DashboardView />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Routes>
+    </AnimatePresence>
+  );
+};
+
+const AppRouter: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <AnimatedRoutes />
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
